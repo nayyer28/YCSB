253ba634d960f24b1ee602d8c369b898eb3e440a
Saahil Nayyer <nayyersaahil@gmail.com>
2024-08-02 21:15:25 +0200

    ycsb redis modubft latencies

--- redis/src/main/java/site/ycsb/db/RedisClient.java
+++ redis/src/main/java/site/ycsb/db/RedisClient.java
@@ -35,10 +35,8 @@ import java.net.Socket;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
 import java.util.Set;
 import java.util.Vector;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import queryMessage.Message;
 import queryMessage.Message.Query;
@@ -50,13 +48,7 @@ import queryMessage.Message.Query;
  */
 public class RedisClient extends DB {
 
-  public int expectedOps;
-
   public static final String INDEX_KEY = "_indices";
-
-  // private int totalOps = 0;
-
-  private AtomicInteger totalOps = new AtomicInteger(0);
 
   private int moduBftClientPort = 10000;
 
@@ -66,17 +58,10 @@ public class RedisClient extends DB {
   private InputStream in;
 
   public void init() throws DBException {
-    Properties props = getProperties();
-    expectedOps = Integer.parseInt(props.getProperty("expectedOps"));
-    // System.out.println("expectedOps: " + expectedOps);
-
     try {
       activeConnection = new Socket("localhost", moduBftClientPort);
-      //System.out.println("so timeout: " + activeConnection.getSoTimeout());
       out = activeConnection.getOutputStream();
       in = activeConnection.getInputStream();
-      // System.out.println(String.format("Connection established with modubft client
-      // at port %d", moduBftClientPort));
     } catch (Exception e) {
       throw new DBException(
           String.format("Failed to establish  connection with modubft client at port %d", moduBftClientPort), e);
@@ -97,31 +82,6 @@ public class RedisClient extends DB {
     return key.hashCode();
   }
 
-  // XXX jedis.select(int index) to switch to `table`
-
-  /*
-   * @Override
-   * public Status read(String table, String key, Set<String> fields,
-   * Map<String, ByteIterator> result) {
-   * if (fields == null) {
-   * StringByteIterator.putAllAsByteIterators(result, jedis.hgetAll(key));
-   * } else {
-   * String[] fieldArray = (String[]) fields.toArray(new String[fields.size()]);
-   * List<String> values = jedis.hmget(key, fieldArray);
-   * 
-   * Iterator<String> fieldIterator = fields.iterator();
-   * Iterator<String> valueIterator = values.iterator();
-   * 
-   * while (fieldIterator.hasNext() && valueIterator.hasNext()) {
-   * result.put(fieldIterator.next(),
-   * new StringByteIterator(valueIterator.next()));
-   * }
-   * assert !fieldIterator.hasNext() && !valueIterator.hasNext();
-   * }
-   * return result.isEmpty() ? Status.ERROR : Status.OK;
-   * }
-   */
-
   @Override
   public Status read(String table, String key, Set<String> fields,
       Map<String, ByteIterator> result) {
@@ -139,23 +99,8 @@ public class RedisClient extends DB {
       }
       resp = sendQuery(args);
     }
-    totalOps.incrementAndGet();
-    checkStatus();
     return resp;
   }
-
-  /*
-   * @Override
-   * public Status insert(String table, String key,
-   * Map<String, ByteIterator> values) {
-   * if (jedis.hmset(key, StringByteIterator.getStringMap(values))
-   * .equals("OK")) {
-   * jedis.zadd(INDEX_KEY, hash(key), key);
-   * return Status.OK;
-   * }
-   * return Status.ERROR;
-   * }
-   */
 
   @Override
   public Status insert(String table, String key,
@@ -168,40 +113,24 @@ public class RedisClient extends DB {
       args[index++] = entry.getKey();
       args[index++] = entry.getValue().toString();
     }
-    sendQuery(args);
-    Status resp = sendQuery("ZADD", INDEX_KEY, Double.toString(hash(key)), key);
-    totalOps.incrementAndGet();
-    checkStatus();
-    return resp;
+    if (sendQuery(args) == Status.OK) {
+      Status resp = sendQuery("ZADD", INDEX_KEY, Double.toString(hash(key)), key);
+      return resp;
+    }
+    return Status.ERROR;
 
   }
-
-  /*
-   * @Override
-   * public Status delete(String table, String key) {
-   * return jedis.del(key) == 0 && jedis.zrem(INDEX_KEY, key) == 0 ? Status.ERROR
-   * : Status.OK;
-   * }
-   */
 
   @Override
   public Status delete(String table, String key) {
-    sendQuery("DEL", key);
-    Status resp = sendQuery("ZREM", key);
-    totalOps.incrementAndGet();
-    checkStatus();
-    return resp;
 
+    if (sendQuery("DEL", key) == Status.OK) {
+      Status resp = sendQuery("ZREM", key);
+      return resp;
+    }
+
+    return Status.ERROR;
   }
-
-  /*
-   * @Override
-   * public Status update(String table, String key,
-   * Map<String, ByteIterator> values) {
-   * return jedis.hmset(key, StringByteIterator.getStringMap(values))
-   * .equals("OK") ? Status.OK : Status.ERROR;
-   * }
-   */
 
   @Override
   public Status update(String table, String key,
@@ -215,29 +144,9 @@ public class RedisClient extends DB {
       args[index++] = entry.getValue().toString();
     }
     Status resp = sendQuery(args);
-    totalOps.incrementAndGet();
-    checkStatus();
     return resp;
 
   }
-
-  /*
-   * @Override
-   * public Status scan(String table, String startkey, int recordcount,
-   * Set<String> fields, Vector<HashMap<String, ByteIterator>> result) {
-   * Set<String> keys = jedis.zrangeByScore(INDEX_KEY, hash(startkey),
-   * Double.POSITIVE_INFINITY, 0, recordcount);
-   * 
-   * HashMap<String, ByteIterator> values;
-   * for (String key : keys) {
-   * values = new HashMap<String, ByteIterator>();
-   * read(table, key, fields, values);
-   * result.add(values);
-   * }
-   * 
-   * return Status.OK;
-   * }
-   */
 
   @Override
   public Status scan(String table, String startkey, int recordcount,
@@ -245,51 +154,24 @@ public class RedisClient extends DB {
     throw new UnsupportedOperationException("Scan is not offered yet");
   }
 
-  /*
-   * private Status sendQuery(String... args) {
-   * System.out.println(String.join(" ", args));
-   * totalOps++;
-   * if (totalOps == expectedOps) {
-   * try {
-   * System.out.println("Thread sleeping for 2s. Ops count: " + totalOps +
-   * " expected ops: " + expectedOps);
-   * Thread.sleep(2000);
-   * } catch (Exception e) {
-   * System.err.println("Error sending query");
-   * }
-   * 
-   * totalOps = 0;
-   * }
-   * return Status.OK;
-   * }
-   */
-
   private Status sendQuery(String... args) {
 
-    /*
-     * if (totalOps.get() > 0) {
-     * return Status.OK;
-     * }
-     */
-
-    /*
-     * System.out.println(args.length);
-     */
     try {
       byte[] msg = transformArgsToProtoMessage(args);
-      // System.out.println("sending bytes: " + msg.length + " totalOps=" + totalOps);
 
       byte[] length = ByteBuffer.allocate(4).putInt(msg.length).array();
       out.write(length);
       out.write(msg);
       out.flush();
-      // System.out.println(String.join(" ", args));
-      // System.out.println("Total sent: " + totalOps);
+      if (checkStatus()) {
+        return Status.OK;
+      }
+      return Status.ERROR;
 
     } catch (Exception e) {
       e.printStackTrace();
+      return Status.ERROR;
     }
-    return Status.OK;
   }
 
   private byte[] transformArgsToProtoMessage(String[] args) {
@@ -302,27 +184,19 @@ public class RedisClient extends DB {
     return data;
   }
 
-  private void checkStatus() {
+  private boolean checkStatus() {
     try {
-      if (totalOps.get() == expectedOps) {
-        int response;
-        // System.out.println("Awaiting response");
-        while ((response = in.read()) != -1) {
-          // System.out.println("response is " + response);
-          if (response == 1) {
-            out.close();
-            in.close();
-            activeConnection.close();
-            break;
-          }
+      int response;
+      while ((response = in.read()) != -1) {
+        if (response == 1) {
+          return true;
         }
-
-        totalOps.set(0);
       }
+      return false;
     } catch (Exception e) {
       e.printStackTrace();
+      return false;
     }
-
   }
 
 }
